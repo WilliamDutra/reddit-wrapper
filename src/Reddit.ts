c67973161e716e39eeb883e0532e90a3792aed70
William Dutra <will.dutra26@gmail.com>
2021-08-26 19:30:13 -0300

    adicionado método do retorno do Modhash chave CSRF da api do reddit

--- src/Reddit.ts
+++ src/Reddit.ts
@@ -3,7 +3,11 @@ import { URLSearchParams } from 'url';
 
 export default class Reddit {
 	
-	private URL: string  = 'https://www.reddit.com';
+	private AUTH_URL: string  = 'https://www.reddit.com';
+	
+	private URL_API: string = 'https://oauth.reddit.com';
+	
+	private URL_SSL: string = 'https://ssl.reddit.com';
 	
 	private Username: string;
 	
@@ -13,25 +17,28 @@ export default class Reddit {
 	
 	private SecretId: string;
 	
+	private UserAgent: string;
+	
 	constructor(args: any) {		
 		this.Username = args.Username;
 		this.Password = args.Password;
 		this.ClientId = args.ClientId;
 		this.SecretId = args.SecretId;
+		this.UserAgent = args.UserAgent;
 	}
 	
-	public async GetBearerToken() : Promise<string> {
+	public async GetBearerToken() : Promise<any> {
 		
 		var UrlEnconded = new URLSearchParams();
-		UrlEnconded.append("grant_type", "password");
-		UrlEnconded.append("username", "Darklance1");
-		UrlEnconded.append("password", "arn13056");
+		UrlEnconded.append('grant_type', 'password');
+		UrlEnconded.append('username', `${this.Username}`);
+		UrlEnconded.append('password', `${this.Password}`);
 		
-		let response = await fetch(`${this.URL}/api/v1/access_token`, {
+		let response = await fetch(`${this.AUTH_URL}/api/v1/access_token`, {
 			method: 'POST',
 			body: UrlEnconded,
 			headers: {
-				'Authorization': 'Basic T04zbTd3akR1WW13MW8wUmloRmJVZzpQX241Uno5TkNNMHMwdFQ1N0tjTG5tRW5kVDJOUnc=',
+				'Authorization': 'Basic ' + new Buffer(`${this.ClientId}:${this.SecretId}`).toString('base64'),
 				'Content-Type': 'application/x-www-form-urlencoded',
 				'User-Agent': 'MeuBot/1.0.0 (http://example.com)'
 			}
@@ -41,5 +48,84 @@ export default class Reddit {
 		
 	}
 	
+	public async GetInfosMe(BearerToken: string): Promise<any> {
+		
+		let response = await fetch(`${this.URL_API}/api/me.json`, {
+			method: 'GET',
+			headers: {
+				'Authorization': `Bearer ${BearerToken}`,
+				'Content-Type': 'application/json',
+				'User-Agent': `${this.UserAgent}`
+			}
+		});
+		
+		return await response.json();
+	}
 	
+	public async GetInfoSubReddit(BearerToken: string, Subreddit: string) : Promise<any> {
+		
+		let response = await fetch(`${this.URL_API}/r/${Subreddit}/about`, {
+			method: 'GET',
+			headers: {
+				'Authorization': `Bearer ${BearerToken}`,
+				'Content-Type': 'application/json',
+				'User-Agent': `${this.UserAgent}`
+			}
+		});
+			
+		return await response.json();
+		
+	}
+	
+	public async  GetNewPostsOfSubReddit(BearerToken: string, Subreddit: string): Promise<any> {
+		
+		//`${this.URL_API}/r/${Subreddit}/new?sort=rising&limit=2`
+		
+		let response = await fetch(`${this.URL_API}/r/${Subreddit}/new`, {
+			method: 'GET',
+			headers: {
+				'Authorization': `Bearer ${BearerToken}`,
+				'Content-Type': 'application/json',
+				'User-Agent': `${this.UserAgent}`
+			}
+		});
+		
+		return await response.json();
+		
+	}
+	
+	public async SearchSubReddit(BearerToken: string, Name: string) : Promise<any> {
+				
+		let response = await fetch(`${this.URL_API}/subreddits/search?q=${encodeURI(Name)}`, {
+			method: 'GET',
+			headers: {
+				'Authorization': `Bearer ${BearerToken}`,
+				'Content-Type': 'application/json',
+				'User-Agent': `${this.UserAgent}`
+			}
+		});
+		
+		return await response.json();
+	}
+
+	
+
+	public async GetModHash() : Promise<any> {
+		
+		let UrlEnconded = new URLSearchParams();
+		UrlEnconded.append('user', `${this.Username}`);
+		UrlEnconded.append('passwd', `${this.Password}`);
+		UrlEnconded.append('api_type', 'json');
+		
+		let response = await fetch(`${this.URL_SSL}/api/login`, {
+			method: 'POST',
+			headers: {
+				'User-Agent': `${this.UserAgent}`
+			},
+			body: UrlEnconded
+		});
+		
+		return response.json();
+	}
+
 }
